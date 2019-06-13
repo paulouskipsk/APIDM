709c582cb0c0f23ec3bc39e40f3208b6bf22e456
Everton Paulouski <everton@pop-os.localdomain>
2019-06-13 02:18:32 -0300

    .6

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,10 +9,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-               ->header('Access-Control-Allow-Origin', 'Origin, *')
-               ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-               ->header('Access-Control-Allow-Headers', '*')
-               ->header('Access-Control-Allow-Credentials',' false');
+               ->header('Access-Control-Allow-Origin', '*')
+               ->header('Access-Control-Allow-Methods', '*')
+               ->header('Access-Control-Allow-Headers', '*');
 
     }
 
