c39411e5b39b529ea514f2daddccfdd2c1b37e39
Everton Paulouski <everton@pop-os.localdomain>
2019-06-13 02:06:13 -0300

    .4

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -72,5 +73,6 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
+        \App\Http\Middleware\Cors::class,
     ];
 }
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,8 +9,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-               ->header('Access-Control-Allow-Origin', '*')
-               ->header('Access-Control-Allow-Methods', '*')
+               ->header('Access-Control-Allow-Origin', 'Origin, *')
+               ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                ->header('Access-Control-Allow-Headers', '*');
 
     }
